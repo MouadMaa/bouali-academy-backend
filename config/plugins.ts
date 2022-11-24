b77177d3e5bf10e70cd23405afa6727e67e3b599
mouadmaa <maaroufimouad00@gmail.com>
2022-11-24 20:12:56 +0100

    remove graphql config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,5 @@
 export default ({ env }) => ({
   ckeditor: true,
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
   upload: {
     config: {
       provider: "cloudinary",
